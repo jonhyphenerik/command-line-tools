4bc73f590a2bf2e4b02c818349b11062bc90ddba
Jon-Erik Nissen <jenissen@me.com>
2020-06-12 21:09:36 -0700

    anonymized config file

--- config.sh
+++ config.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #user-specific configuration information referenced in other files. Variables are initialized at start of each bash script
 
-filepath='/Users/jon-eriknissen/devmtn'
+filepath='/Users/username'
 #filepath of the directory that contains tools. no trailing /
 
